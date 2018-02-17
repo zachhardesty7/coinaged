08e2a3df97e5bf1dde21853c16286b174fb32bff
Zach Hardesty <zachhardesty@utexas.edu>
2018-02-17 16:42:45 -0600

    fix server issues

--- mongodbapi.py
+++ mongodbapi.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, request
 from flask_restful import Resource, Api
-from flask.ext.pymongo import PyMongo
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 # from json import dumps
