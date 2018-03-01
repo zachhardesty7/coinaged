0da02833e1770be306d5a7e920eaa0c2162e17ee
Zach Hardesty <zachhardesty@utexas.edu>
2018-03-01 16:06:00 -0600

    airdropped token not in DB yet

--- coinagedPyMongo.py
+++ coinagedPyMongo.py
@@ -77,6 +77,8 @@ def getPortfolio(usersDB, transactionsDB, tradesDB, timestamp=int(time())):
     tickers = getTickers()
     # probably use some type of caching to speed up api response time
     tickerPrices = getTickerPrices(tickers, timestamp)
+    tickers = updateTickers(tickers, tickerPrices)
+
     portfolioPrinciple = getPortfolioPrinciple(transactionsDB, timestamp)
     if(abs(timestamp - LAST_TIMESTAMP) > 3600):
         updateTradeDB(tradesDB, transactionsDB, tickers)
@@ -198,6 +200,18 @@ def getTickers():
     LOGGER.info(currentFuncName() + ': took ' + str(getUnixTimeLog() - start) + ' seconds')
 
     return tickers
+
+
+def updateTickers(tickers, tickerPrices):
+    print(tickers)
+    print(tickerPrices)
+
+    newTickers = []
+
+    for ticker, price in tickerPrices:
+        newTickers.append(ticker)
+
+    return newTickers
 
 
 def getTickerPrices(tickers, timestamp=int(time())):
